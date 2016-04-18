0efeebe247a5cfa431dcd27cccb6da7cea6b9153
Andrej Antas <andrej@antas.cz>
2016-04-18 11:29:53 +0200

    First commit with router

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+var React = require('react')
+
+var Home = React.createClass({
+  render: function() {
+    return (
+      <div className='text-center'>
+        Search by Github Username Above
+      </div>
+    )
+  }
+});
